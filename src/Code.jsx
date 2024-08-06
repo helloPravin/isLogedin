e5c7909eb9f8cc073df0b1dfb49e6aeaaf21be1c
helloPravin <mavinCoder@outlook.com>
2024-08-06 13:10:09 +0530

    Modified Code component

--- src/Code.jsx
+++ src/Code.jsx
@@ -3,7 +3,7 @@ import { CodeBlock } from "react-code-block";
 function Code(props) {
   return (
     <CodeBlock code={props.projectCode} language="jsx">
-      <CodeBlock.Code className="mx-4 text-wrap rounded-3xl border border-black p-6 text-xs sm:mx-24">
+      <CodeBlock.Code className="mx-2 text-wrap rounded-3xl border border-black p-6 text-xs sm:mx-24">
         <div className="table-row">
           <CodeBlock.LineNumber className="table-cell select-none pr-4 text-right text-sm text-gray-500" />
           <CodeBlock.LineContent className="table-cell">
